a7f307edc08aa3a61cec521f543e78f2fe98b2d8
Anna Abdiyu <annaply156@gmail.com>
2025-07-29 17:10:55 +0300

    ET-cinema

--- src/services/api.js
+++ src/services/api.js
@@ -7,8 +7,8 @@ export const getpopularMovies=async()=>{
     return data.results
 };
 
-export const getpopularMovies=async()=>{
-    const response = await fetch('${BASE_URL}/movie/popular?api_key=${API_KEY}');
+export const searchMovies=async(query)=>{
+    const response = await fetch('${BASE_URL}/search/movie?api_key=${API_KEY}&query=${encodeURIComponent(query)}');
     const data = await response.json()
     return data.results
 };
